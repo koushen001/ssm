338b512f0a86cd1bc31400043a37d685d63c4888
cike <875569847@qq.com>
2017-09-03 15:16:27 +0800

    加入swagger

--- ssm-web/src/main/java/com/cike/web/UserController.java
+++ ssm-web/src/main/java/com/cike/web/UserController.java
@@ -1,24 +1,27 @@
 package com.cike.web;
 
 
+import com.cike.common.ServerResponse;
 import com.cike.entity.User;
 import com.cike.service.IUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 用户管理
  * </p>
  *
  * @author cike
  * @since 2017-09-02
  */
+@Api(value = "用户管理")
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -26,10 +29,33 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
-    @RequestMapping("/list")
+    @ApiOperation(value = "查询用户", httpMethod = "GET")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
     @ResponseBody
-    public List<User> list(){
-        return  userService.selectList(null);
+    public ServerResponse list() {
+        return ServerResponse.createBySuccess(userService.selectList(null));
+    }
+
+    @ApiOperation(value = "添加或修改用户", httpMethod = "POST")
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse save(User user) {
+        boolean b = userService.insertOrUpdate(user);
+        if (b) {
+            return ServerResponse.createBySuccessMessage("操作成功");
+        }
+        return ServerResponse.createByErrorMessage("操作失败");
+    }
+
+    @ApiOperation(value = "删除用户", httpMethod = "GET")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse delete(@PathVariable Integer id) {
+        boolean b = userService.deleteById(id);
+        if (b) {
+            return ServerResponse.createBySuccessMessage("操作成功");
+        }
+        return ServerResponse.createByErrorMessage("操作失败");
     }
 
 }
